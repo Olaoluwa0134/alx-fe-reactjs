11b547cac8871bc7bf192a7d114b2e06f1fd0c53
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-02-16 23:55:23 +0100

    task upload

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
