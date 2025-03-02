131643a3e82776170dc9ce458c9ac8e108a7b8fd
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-03-02 22:36:23 +0100

    add useNavigate

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,6 +1,9 @@
-import { useRecipeStore } from "./recipeStore";
+import { useRecipeStore } from "../recipeStore";
+import { useNavigate } from "react-router-dom";
 
 export const DeleteRecipeButton = ({ recipeId }) => {
+  const navigate = useNavigate();
+
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   return <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>;
