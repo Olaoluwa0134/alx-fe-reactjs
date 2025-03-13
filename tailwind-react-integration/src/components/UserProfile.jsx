e8314f35f189409f7185f677184bd1135f8e0e2d
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-03-13 23:44:26 +0100

    task upload

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration:200 ease-in-out">
@@ -19,3 +20,23 @@ function UserProfile() {
 ``;
 
 export default UserProfile;
+=======
+function UserProfile() {
+  return (
+    <div className="user-profile b-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full w-36 h-36 mx-auto"
+      />
+      <h1 className="md-text-xl sm-text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md-text-base sm-text-sm">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
+>>>>>>> 86e5464 (task upload)
